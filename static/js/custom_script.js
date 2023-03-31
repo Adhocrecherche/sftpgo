dc521a6476cece706b754976ef7083ba1636da58
Rondet <Mickael.Rondet@adhoc-recherche.com>
2023-03-31 15:05:12 -0400

    Personnalisation fichiers js et template html

---
+++ static/js/custom_script.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function (){
+	$(".nav-link").find("span:contains('My Files')").html("Mes Fichiers | My Files");
+	$(".nav-link").find("span:contains('Shares')").text("Partages | Shares");
+	$(".nav-link").find("span:contains('My Profile')").text("Mon Profil | My Profile");
+});
